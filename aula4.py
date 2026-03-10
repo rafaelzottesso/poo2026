22a3d178205157fe86030f1ba2f142402f2f8afe
Professor <rafael.zottesso@ifpr.edu.br>
2026-03-10 09:11:51 -0300

    Melhoria do algoritmo para usar laços de repetição

--- aula4.py
+++ aula4.py
@@ -1,13 +1,8 @@
 alunos = [""] * 5
-alunos[0] = "Fulano de Tal"
-alunos[1] = "Ciclano"
-alunos[2] = input("Nome do aluno: ")
-alunos[3] = input("Nome do aluno: ")
-alunos[4] = input("Nome do aluno: ")
+for i in range(0, len(alunos)):
+    alunos[i] = input("Nome: ")
 
 print("Lista de alunos:")
-print("Aluno 1:", alunos[0])
-print("Aluno 2:", alunos[1])
-print("Aluno 3:", alunos[2])
-print("Aluno 4:", alunos[3])
-print("Aluno 5:", alunos[4])
+i = 0
+while(i<5):
+    print(f"Aluno {i+1}: {alunos[i]}")
