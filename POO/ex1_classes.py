389ac2f59796a86de7a7c4c4f651d579708667b1
Professor <rafael.zottesso@ifpr.edu.br>
2026-04-07 10:27:51 -0300

    Comentários explicativos no código e classes

--- POO/ex1_classes.py
+++ POO/ex1_classes.py
@@ -1,41 +1,54 @@
 # Criação da classe aluno
 class Aluno:
 
-    # Método construtor do aluno
+    # Método construtor do aluno com os atributos que são obrigatórios: nome, idade e curso
     def __init__(self, nome, idade, curso):
+        # O self indica que os atributos vão ter valores diferentes para cada objeto
+        # Porque um aluno será o Pedro, o João, a Maria, etc
         self.nome = nome
         self.idade = idade
         self.curso = curso
-        self.ra = ""
-        # self.notas = notas
+        self.ra = "" # opcional
+        self.notas = [] # opcional, mas será uma lista (vazia por enquanto)
 
-    # Método adicional
+    # Método para exibir os dados do aluno
+    # Todo método precisa do self para poder acessar os atributos de cada objeto
     def apresentar(self):
         print(f"Olá, meu nome é {self.nome}, tenho {self.idade} anos e faço o curso de {self.curso}.")
-        # while(self.ra == ""):
-        #     print("Esse aluno não tem RA.")
-        #     self.ra = input("Infome o RA: ")
-       
-        print(f"O RA é: {self.ra}")
+        # Verifica se já tem o RA do aluno e exibe uma mensagem na tela
+        if(self.ra == ""):
+            print("Esse aluno não tem RA.")
+        else:
+            print(f"O RA é: {self.ra}")
     
-    # Método adicional
+    # Percorre a lista de notas, independente do tamanho dela
+    # Faz a soma das notas e depois a média conforme a quantidade de notas existentes (len())
     def calcular_media(self):
         soma = 0.0
         for i in range(0, len(self.notas)):
-            soma += self.notas[i]
-        media = soma/len(self.notas)
-        return media   
+            soma += self.notas[i] # acrescente o valor das notas à variável soma
+        media = soma/len(self.notas) # faz a média
+        return media # retorna a média e não exibe automaticamente na tela
     
+
+# Cria uma classe para a turma    
 class Turma:
 
+    # A turma tem nome e ano obrigatórios
     def __init__(self, nome, ano):
         self.nome = nome
         self.ano = ano
-        self.estudantes = []
+        self.estudantes = [] # Será uma lista de estudantes preenchida com o .append(valor)
 
+    # Exibe os dados da turma na tela
     def apresentar(self):
         print(f"{self.nome} - {self.ano}")
 
+    # Método para percorrer a lista de estudantes e exibir seus dados na tela
     def exibir_estudantes(self):
+        # Percorre a lista de estudantes pelos valores e NÃO pelo índice igual o for i in range faz
+        # a variável aluno recebe o valor que está armazenado e não os índices 0, 1, 2, etc igual o "i"
         for aluno in self.estudantes:
+            # como estudantes é uma lista com objetos, podemos acessar o método de um objeto
+            # no caso de Aluno, tem o método apresentar() que exibe as informações do aluno na tela
             aluno.apresentar()
--- POO/ex1_principal.py
+++ POO/ex1_principal.py
@@ -1,22 +1,30 @@
 # from seu_arquivo import SuaClasse
+# Importa as classes criadas em outro arquivo
 from ex1_classes import Aluno, Turma 
 
+# Cria "na mão" três objetos
+# se não teria que fazer um monte de input
 pedro = Aluno("Pedro", 15, "Info")
 joppew = Aluno("João Almeida", 67, "Info")
 miguel = Aluno("Miguel Garcia Sorrilhia Neto", 15, "Info")
 
+# Cria uma turma
 info2024 = Turma("INFO24", 2024)
-info2024.estudantes.append(joppew)
-info2024.estudantes.append(pedro)
-info2024.estudantes.append(miguel)
+# Usa o .append no atributo estudantes que é uma lista vazia []
+info2024.estudantes.append(joppew) # adiciona um objeto nela
+info2024.estudantes.append(pedro) # adiciona outro objeto nela
+info2024.estudantes.append(miguel) # adiciona mais um objeto nela
+# Executa o método que percorre a lista e exibe as informaçõies na tela
 info2024.exibir_estudantes()
 
-
+# Outra forma de criar objetos, mas usando o input para o usuário criar com seus dados desejados
 # nome = input("Qual seu nome: ")
 # idade = int(input("Qual sua idade: "))
 # curso = input("Qual o curso que você está cursando: ")
 
+# Cria o objeto com os dados fornecidos pelo usuário
 # alun1 = Aluno(nome, idade, curso, [7, 10, 8])
+# Executa o método
 # alun1.apresentar()
 # ra = input("Qual seu RA: ")
 # alun1.ra = ra
